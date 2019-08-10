b42264742a3b2b824434b94abc44a75f7d1045d3
yukiTakezawa <courage.t0629@gmail.com>
2019-08-11 00:59:38 +0900

    load_data()

--- src/functions.py
+++ src/functions.py
@@ -1,13 +1,14 @@
 import math
 import torch
 from ast import literal_eval
+from molecule import *
 
 def load_data():
     data_num = 1112
 
-    graph_labels_file = open('../dataset/PROTEINS/PROTEINS_graph_labels.txt')
-    node_labels_file = open('../dataset/PROTEINS/PROTEINS_node_labels.txt')
-    node_attributes_file = open('../dataset/PROTEINS/PROTEINS_node_attributes.txt')
+    graph_label_file = open('../dataset/PROTEINS/PROTEINS_graph_labels.txt')
+    node_label_file = open('../dataset/PROTEINS/PROTEINS_node_labels.txt')
+    node_attribute_file = open('../dataset/PROTEINS/PROTEINS_node_attributes.txt')
     graph_idx_file = open('../dataset/PROTEINS/PROTEINS_graph_indicator.txt')
     adjacency_file = open('../dataset/PROTEINS/PROTEINS_A.txt')
 
@@ -46,8 +47,40 @@ def load_data():
             else:
                 break
         tmp_sum1 = tmp_sum
-    
-                
+
+    # check node feature
+    node_dim = 4 # dimension of node
+    node_list = []
+    for graph_idx in range(data_num):
+        nodes = torch.zeros(graph_size_list[graph_idx], node_dim)
+        
+        for node_idx in range(graph_size_list[graph_idx]):
+            node_label = int(node_label_file.readline())
+            node_attribute = float(node_attribute_file.readline())
+            if (node_label == 0):
+                nodes[node_idx] = torch.tensor([1, 0, 0, node_attribute])
+            elif (node_label == 1):
+                nodes[node_idx] = torch.tensor([0, 1, 0, node_attribute])
+            else:
+                nodes[node_idx] = torch.tensor([0, 0, 1, node_attribute])
+        node_list.append(nodes)
+    print(node_list[0])
+
+    # set molecule class
+    molecule_list = []
+    label_list = []
+    for graph_idx in range(data_num):
+        molecule_list.append(Molecule(graph_list[graph_idx], node_list[graph_idx]))
+        label_list.append(int(graph_label_file.readline()))
+
+    graph_label_file.close()
+    node_label_file.close()
+    node_attribute_file.close()
+    graph_idx_file.close()
+    adjacency_file.close()
+
+    return molecule_list, label_list
+
 def sigmoid(s):
     return 1.0/(1.0 + math.exp(-s))
 
