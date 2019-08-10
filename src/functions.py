c62a63cd4398c77ab4eef1d5bbcbc9759d797896
yukiTakezawa <courage.t0629@gmail.com>
2019-08-11 00:21:52 +0900

    load adjacency matrix

--- src/functions.py
+++ src/functions.py
@@ -1,33 +1,53 @@
 import math
 import torch
+from ast import literal_eval
 
 def load_data():
-    data_num = 188
-    
-    edge_file = open('../dataset/Mutag/Mutag.edges')
-    graph_labels_file = open('../dataset/Mutag/Mutag.graph_labels')
-    node_labels_file = open('../dataset/Mutag/Mutag.node_labels')
-    graph_idx_file = open('../dataset/Mutag/Mutag.graph_idx')
-    link_labels_file = open('../dataset/Mutag/Mutag.link_labels')
+    data_num = 1112
 
-    # check node size
+    graph_labels_file = open('../dataset/PROTEINS/PROTEINS_graph_labels.txt')
+    node_labels_file = open('../dataset/PROTEINS/PROTEINS_node_labels.txt')
+    node_attributes_file = open('../dataset/PROTEINS/PROTEINS_node_attributes.txt')
+    graph_idx_file = open('../dataset/PROTEINS/PROTEINS_graph_indicator.txt')
+    adjacency_file = open('../dataset/PROTEINS/PROTEINS_A.txt')
+
+    # check graph size
     tmp0, tmp1 = 1, 1
+    graph_list = [] # list of adjacency matrix
+    graph_size_list = [] # list of total number of nodes that each graph has
+    
     for graph_idx in range(data_num):
-        node_size = 0
+        graph_size = 1 # total number of nodes that graph has
 
         while True:
-            tmp1 = graph_idx_file.readlines()
+            tmp1 = int(graph_idx_file.readline())
             if (tmp0 == tmp1):
-                node_size += 1
+                graph_size += 1
                 tmp0 = tmp1
             else:
                 tmp0 = tmp1
                 break
 
-        # check ajacency matric
-        graph = torch.zeros(node_size, node_size)
+        # append adjacency matric
+        graph_list.append(torch.zeros(graph_size, graph_size))
+        graph_size_list.append(graph_size)
         
+    # chack adjacency matrix
+    tmp_sum = 0
+    tmp_sum1 = 0
+    for graph_idx in range(data_num):
+        tmp_sum += graph_size_list[graph_idx]
+        
+        while True:
+            edge = literal_eval(adjacency_file.readline())
+            if ((edge[0] <= tmp_sum) & (edge[1] <= tmp_sum)):
+                graph_list[graph_idx][edge[0] - tmp_sum][edge[1] - tmp_sum] = 1
+                graph_list[graph_idx][edge[1] - tmp_sum][edge[0] - tmp_sum] = 1
+            else:
+                break
+        tmp_sum1 = tmp_sum
     
+                
 def sigmoid(s):
     return 1.0/(1.0 + math.exp(-s))
 
